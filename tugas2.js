82f1c97e026147845e6675e79cd662aa03e21e57
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:18:37 +0700

    submit

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+function tugas2(){
+
+var namber = 20;
+var setring = "tulisan";
+var bulean = true;
+var nul = null;
+var andifain;
+
+console.log(namber);
+console.log(setring);
+console.log(bulean);
+console.log(nul);
+console.log(andifain);
+
+}
+
+tugas2();
